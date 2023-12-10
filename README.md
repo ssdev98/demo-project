cbf06cd6bbd4934092dddb86d1278df8bdc2e5db
Soufiane Saadouni <soufiane_dev@outlook.com>
2023-12-10 21:47:30 +0100

    feat: add new examples

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+
 # demo-project
 
 This is a readme file
+
+## DevOps
+
+- <https://docs.github.com/en/actions/learn-github-actions>
+- <https://github.com/solutions/ci-cd/>
+- <https://resources.github.com/devops/>
+- <https://github.com/actions/checkout>
+- <https://docs.fastlane.tools/best-practices/continuous-integration/github/>
--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,22 +1,107 @@
 #!/bin/bash
 
+
 echo "Hello Soufiane from bash script!"
+
+#
+#
+#
+#
+#
 
 arr1=(item1 item1 item3 item4)
 
-echo ${arr1[0]}
+echo "${arr1[0]}"
 
-echo ${arr1[1]}
+echo "${arr1[1]}"
 
-echo ${arr1[2]}
+echo "${arr1[2]}"
 
-echo ${arr1[3]}
+echo "${arr1[3]}"
 
 # To print all elements of the array
-echo ${arr1[*]}
+echo "${arr1[*]}"
+
+#
+#
+#
+#
+#
+
+# bash '/home/soufiane/Projects/Devops/demo-project/scripts/script1.sh' ARG1 ARG2
 
 # Print commandline argument number 1
-echo "First command line argument->" $1
+echo "First command line argument->" "$1"
 
 # Print commandline argument number 2
-echo "Second command line argument->" $2
+echo "Second command line argument->" "$2"
+
+#
+#
+#
+#
+#
+
+x=1998
+echo ${x}
+
+#
+#
+#
+#
+#
+
+a=10
+b=1000
+
+if [ $a = $b ]; then
+    echo "a is equal to b"
+else
+    echo "a is not equal to b"
+fi
+
+#
+#
+#
+#
+#
+
+echo "Current directory = $(pwd)"
+
+#
+#
+#
+#
+#
+
+print_message() {
+    echo "This is a function test!"
+}
+
+print_message
+
+#
+#
+#
+#
+#
+
+for i in {i..10}; do
+    echo "Item $i"
+done
+
+#
+#
+#
+#
+#
+
+# https://devhints.io/bash
+
+#
+#
+#
+#
+#
+
+exit 0
